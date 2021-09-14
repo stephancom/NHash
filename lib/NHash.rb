e61b2085cb08a0dab632292a40e603d8035e0504
stephan.com <stephan@stephan.com>
2021-09-13 21:41:51 -0400

    getting started from base 3D

--- lib/NHash.rb
+++ lib/NHash.rb
@@ -2,7 +2,31 @@
 
 require_relative "NHash/version"
 
-module NHash
+class NHash
   class Error < StandardError; end
-  # Your code goes here...
+
+  def initialize
+    @hash = Hash.new { |hash, key| hash[key] = Hash.new { |inhash, inkey| inhash[inkey] = {} } }
+  end
+
+  # it may be useful to extend this to allow for params to be omitted
+  def [](one, two, three)
+    one, two, three = symbolize(one, two, three)
+    @hash[one][two][three]
+  end
+
+  def []=(one, two, three, value)
+    one, two, three = symbolize(one, two, three)
+    @hash[one][two][three] = value
+  end
+
+  def to_h
+    @hash
+  end
+
+  private
+
+  def symbolize(*inputs)
+    inputs.map { |n| n.respond_to?(:to_sym) ? n.to_sym : n }
+  end
 end
--- spec/NHash_spec.rb
+++ spec/NHash_spec.rb
@@ -5,7 +5,45 @@ RSpec.describe NHash do
     expect(NHash::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  context 'basics' do
+    subject(:nhash) { described_class.new }
+
+    context 'assignment' do    
+      it 'allows assignment to n dimensions' do
+        expect {
+          nhash[:a, :b, :c] = 4
+        }.not_to raise_error
+      end
+
+      it 'fails on too many dimensions' do
+        expect {
+          nhash[:a, :b, :c, :d] = 5
+        }.to raise_error ArgumentError
+      end
+
+      it 'fails on too few dimensions' do
+        expect {
+          nhash[:a, :b] = 3
+        }.to raise_error ArgumentError
+      end
+    end
+
+    it { expect(nhash.is_a?(Hash)).to be false }
+    it { expect(nhash.to_h.is_a?(Hash)).to be true }
+
+    context 'with data stored' do
+      before do
+        nhash[:a, :b, :c] = 42
+        nhash[:red, :green, :blue] = 69        
+      end
+
+      it { expect(nhash[:a, :b, :c]).to eq 42 }
+      it { expect(nhash[:red, :green, :blue]).to eq 69 }
+
+      it 'to_h returns a nested hash with the data' do
+        expect(nhash.to_h).to match( a: { b: { c: 42 } },
+                                   red: { green: { blue: 69 } })
+      end
+    end
   end
 end
